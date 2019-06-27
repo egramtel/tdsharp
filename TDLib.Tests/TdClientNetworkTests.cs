c1a067712707fcb2e4d678a386779dda6d01169b
Sergey Khabibullin <sergey@khabibullin.com>
2019-06-27 19:58:09 +0300

    Add tests

---
+++ TDLib.Tests/TdClientNetworkTests.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using TdLib;
+using Xunit;
+
+namespace TDLib.Tests
+{
+    public class TdClientNetworkTests
+    {
+        [Fact]
+        public async Task Execute_WhenNetworkCallIsMade_ReturnsOk()
+        {
+            using (var client = new TdClient())
+            {
+                var result = await client.ExecuteAsync(new TdApi.TestNetwork());
+                
+                Assert.NotNull(result);
+            }
+        }
+    }
+}
