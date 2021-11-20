8df7e13e10e6c9f2008a1071e487dc557d0b4084
Sergey Khabibullin <sergey@khabibullin.com>
2021-11-20 09:31:46 +0300

    Add logging extensions

---
+++ TDLib/Bindings/TdLibBindingsExtensions.cs
@@ -0,0 +1,18 @@
+using TdLib.Bindings;
+
+namespace TDLib.Bindings
+{
+    public static class TdLibBindingsExtensions
+    {
+        public static void SetLogFilePath(this ITdLibBindings bindings, string path)
+        {
+            var ptr = Interop.StringToIntPtr(path);
+            bindings.SetLogFilePath(ptr);
+        }
+
+        public static void SetLogVerbosityLevel(this ITdLibBindings bindings, TdLogLevel level)
+        {
+            bindings.SetLogVerbosityLevel((int)level);
+        }
+    }
+}
