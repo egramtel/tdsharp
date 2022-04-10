f193af9d35e18f604417b2cf7c01b57722889b7c
Friedrich von Never <friedrich@fornever.me>
2022-04-10 17:04:03 +0700

    CI: NuGet automation

---
+++ Scripts/Get-Version.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    [string] $RefName
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+Write-Output "Determining version from ref $RefName…"
+if ($RefName -match '^refs/tags/v') {
+    $version = $RefName -replace '^refs/tags/v', ''
+    Write-Output "Pushed ref is a version tag, version: $version"
+} else {
+    $propsFilePath = "$PSScriptRoot/../Directory.Build.props"
+    [xml] $props = Get-Content $propsFilePath
+    $version = $props.Project.PropertyGroup.Version
+    Write-Output "Pushed ref is a not version tag, get version from $($propsFilePath): $version"
+}
+
+Write-Output "::set-output name=version::$version"
