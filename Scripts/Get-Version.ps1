e53eac88ef65f20816c7bb1ff2574702f7c65bfd
Friedrich von Never <friedrich@fornever.me>
2023-12-29 19:03:49 +0100

    CI: upgrade from node12 and use env files instead of set-output

--- Scripts/Get-Version.ps1
+++ Scripts/Get-Version.ps1
@@ -5,15 +5,15 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-Write-Output "Determining version from ref $RefName…"
+Write-Host "Determining version from ref $RefName…"
 if ($RefName -match '^refs/tags/v') {
     $version = $RefName -replace '^refs/tags/v', ''
-    Write-Output "Pushed ref is a version tag, version: $version"
+    Write-Host "Pushed ref is a version tag, version: $version"
 } else {
     $propsFilePath = "$PSScriptRoot/../Directory.Build.props"
     [xml] $props = Get-Content $propsFilePath
     $version = $props.Project.PropertyGroup.Version
-    Write-Output "Pushed ref is a not version tag, get version from $($propsFilePath): $version"
+    Write-Host "Pushed ref is a not version tag, get version from $($propsFilePath): $version"
 }
 
-Write-Output "::set-output name=version::$version"
+Write-Output "version=$version"
