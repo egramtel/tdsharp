c332472cfec217ea5da6e0031f48ad79e639b529
Friedrich von Never <friedrich@fornever.me>
2025-11-09 23:07:08 +0100

    CodeGen: fix errors and warnings after migration to .NET 9

--- TdLib.CodeGen/Program.fs
+++ TdLib.CodeGen/Program.fs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 tdsharp contributors <https://github.com/egramtel/tdsharp>
+// SPDX-FileCopyrightText: 2024-2025 tdsharp contributors <https://github.com/egramtel/tdsharp>
 //
 // SPDX-License-Identifier: MIT
 
@@ -7,16 +7,16 @@ module TdLib.CodeGen.Program
 open System.IO
 
 [<EntryPoint>]
-let main args =
-    let writeFile folder name source =
+let main _ =
+    let writeFile folder name (source: string) =
         if not (Directory.Exists(folder)) then
             Directory.CreateDirectory(folder) |> ignore
         File.WriteAllText(folder + "/" + name, source)
 
-    for (name, source) in Generator.generateAllTypes() do
+    for name, source in Generator.generateAllTypes() do
         writeFile "Objects" (name + ".cs") source
 
-    for (name, source) in Generator.generateAllFuncs() do
+    for name, source in Generator.generateAllFuncs() do
         writeFile "Functions" (name + ".cs") source
 
     0
